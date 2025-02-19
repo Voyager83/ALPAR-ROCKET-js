d60ff222dbdec833ea88eee281da3292bd87bc9a
Nicollas R Deluca M <nicollas83@gmail.com>
2025-02-19 15:40:13 -0300

    quarta feira, seguindo na lista de loop

---
+++ extras/ex4-soma-impares.js
@@ -0,0 +1,4 @@
+// Faça um programa em C para solicitar a entrada de um número positivo x. Crie uma 
+// repetição para somar todos os números ímpares de 1 até x. Mostre o valor da soma 
+// obtida. 
+
